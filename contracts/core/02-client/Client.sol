72d4b71a9dfe4788220328c49f91fda19a975a8e
chengwenxi <vincent.ch.cn@gmail.com>
2021-08-19 10:11:37 +0800

    switch to ts and add test example

--- contracts/core/02-client/Client.sol
+++ contracts/core/02-client/Client.sol
@@ -7,13 +7,21 @@ import "openzeppelin-solidity/contracts/security/ReentrancyGuard.sol";
 
 contract ClientManager is Ownable, ReentrancyGuard {
     // chain_name -> IClient implementation address
-    mapping(string => IClientState) public CLIENTS;
+    mapping(string => address) private CLIENTS;
 
-    function updateHeader(string memory chainName, bytes calldata header)
+    function createClient(string calldata chainName, address client) external {
+        CLIENTS[chainName] = client;
+    }
+
+    function getClient(string calldata chainName) public view returns (address){
+        return CLIENTS[chainName];
+    }
+
+    function updateHeader(string calldata chainName, bytes calldata header)
         external
         nonReentrant
     {
-        IClientState client = CLIENTS[chainName];
+        IClientState client = IClientState(CLIENTS[chainName]);
         client.checkHeaderAndUpdateState(header);
     }
 }
---
+++ test/Client.spec.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat"
+import { Signer } from "ethers"
+import chai from "chai";
+
+import { ClientManager } from '../typechain'
+
+const { expect } = chai;
+
+describe('Client', () => {
+    let accounts: Signer[]
+    let clientManager: ClientManager
+
+    before('deploy ClientManager', async () => {
+        accounts = await ethers.getSigners();
+
+        const factory = await ethers.getContractFactory('ClientManager', accounts[0])
+        clientManager = (await factory.deploy()) as ClientManager
+    })
+
+
+    it("add client", async function () {
+        let address = await accounts[0].getAddress()
+        await clientManager.createClient("irishub", address)
+
+        let irishubResult = await clientManager.getClient("irishub")
+        expect(irishubResult).to.eq(address)
+    })
+})
