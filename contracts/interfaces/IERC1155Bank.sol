6b289492a54fd9a006d559f3469b3c4046904315
mindcarver <626984947@qq.com>
2021-10-19 15:01:03 +0800

    move originNft

--- contracts/interfaces/IERC1155Bank.sol
+++ contracts/interfaces/IERC1155Bank.sol
@@ -3,17 +3,6 @@ pragma solidity ^0.6.8;
 pragma experimental ABIEncoderV2;
 
 interface IERC1155Bank {
-    /*
-        keep track of class: tokenId -> nft/{source-chain}/{dest-chain}/nftclass
-        keep track of id :   tokenId -> id
-        keep track of uri :  tokenId -> uri
-    */
-    struct OriginNFT {
-        string class;
-        string id;
-        string uri;
-    }
-
     function mint(
         address account,
         uint256 id,
