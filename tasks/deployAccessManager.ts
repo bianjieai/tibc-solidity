41959ab45688b508f10032af29f72b9bf438c833
mindcarver <626984947@qq.com>
2021-10-21 18:38:57 +0800

    add deployAccessManager task

---
+++ tasks/deployAccessManager.ts
@@ -0,0 +1,17 @@
+import "@nomiclabs/hardhat-web3";
+import { ethers } from "hardhat";
+import { task, types } from "hardhat/config"
+
+const ACCESS_MANAGER_ADDRES = process.env.ACCESS_MANAGER_ADDRES;
+
+task("deployAcessManager", "Deploy acessManager")
+    .setAction(async (taskArgs, hre) => {
+        const accessManagerFactory = await hre.ethers.getContractFactory('AccessManager')
+
+        const accessManager = await hre.upgrades.deployProxy(accessManagerFactory);
+        await accessManager.deployed();
+        console.log("AccessManager deployed to:", accessManager.address);
+        console.log("export ACCESS_MANAGER_ADDRES=%s", accessManager.address);
+    });
+
+    module.exports = {};
